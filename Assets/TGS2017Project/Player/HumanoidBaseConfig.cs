2a814cc0d9c7fbbec04cb1e19f407529f697ba21
Nozawa <n.shota1115@gmail.com>
2017-06-09 13:30:20 +0900

    エネルギー残量UI変更

---
+++ Assets/TGS2017Project/Player/HumanoidBaseConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyGame/Create HumanoidBaseConfig", fileName = "HumanoidBaseConfig")]
+public class HumanoidBaseConfig : ScriptableObject {
+	public string m_InputHorizontal = "HorizontalL";
+	public string m_InputVertical = "VerticalL";
+	public string m_InputRotation = "HorizontalR";
+	public string m_InputBoost = "Boost";
+	public string m_InputJump = "Jump";
+	public string m_InputCharge = "Charge";
+}
