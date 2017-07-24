7c047e40089167b1140e22b21b17f1169bd47929
maeda <mh2454115@icloud.com>
2017-07-24 21:38:40 +0900

    StageSelectのスクリプトを修正、ExStageのボタンを追加

---
+++ Assets/TGS2017Project/Scenes/SelectScene/Parts.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour {
+    public Vector3 m_Position;
+    public Quaternion m_Rotation;
+}
